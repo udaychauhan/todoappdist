eb1a1fe3f138df8051f3638ce2f0e6d0f68f7d3b
udaychauhan <uday.myplanet@gmail.com>
2018-06-11 21:25:30 +0530

    apidoc and read me added

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Todo App",
+  "version": "0.0.1",
+  "description": "Documentation for TODO APP",
+  "title": "TODO App APIs",
+  "url": "http://todoapi.udaychauhan.info",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-06-11T15:51:54.277Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
